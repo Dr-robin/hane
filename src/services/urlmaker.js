736aa78856ed821ed314ae889929f4b48b7db010
robin* <robin_maki@planet.moe>
2018-05-15 21:26:46 +0900

    프로필 사진 URLMaker 추가

--- src/services/urlmaker.js
+++ src/services/urlmaker.js
@@ -3,5 +3,13 @@ const config = require('../lib/config');
 module.exports = {
 	profileURL(username) {
 		return config.getValue('web.rootURL', 'http://localhost:8734') + '/@' + username;
+	},
+	avatar(id) {
+		if(id) {
+			return config.getValue('web.storageURL', 'http://localhost:8734') + '/' + id;
+		}
+		else {
+			return config.getValue('web.rootURL', 'http://localhost:8734') + '/assets/noavatar.png';
+		}
 	}
 };
