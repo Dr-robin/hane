b2fb394506eaed6d9503f5f10005339b22f847d7
robin* <robin_maki@planet.moe>
2018-05-15 20:30:20 +0900

    isLocal을 domain으로 수정

--- src/models/profile.js
+++ src/models/profile.js
@@ -5,9 +5,9 @@ const Account = require('./account');
 const model = db.define('profile', {
 	id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
 	name: {type: Sequelize.STRING(20), unique: true},
-	isLocal: {type: Sequelize.BOOLEAN}
+	domain: {type: Sequelize.STRING}
 }, {
-
+	indexes: [{fields: ['domain']}]
 });
 
 Account.Profile = Account.hasMany(model);
