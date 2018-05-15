35960da511c6d807854094d55e7b3436ed7b1607
robin* <robin_maki@planet.moe>
2018-05-15 21:01:33 +0900

    사용자명 제약 조건 추가

--- src/schema/account.js
+++ src/schema/account.js
@@ -8,6 +8,6 @@ module.exports = {
 	register: {
 		email: joi.string().email().required(),
 		password: joi.string().min(6).required(),
-		username: joi.string().min(4).max(20).required()
+		username: joi.string().min(4).max(20).regex(/^[a-zA-Z0-9_]+$/).required()
 	}
 };
