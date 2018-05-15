b20bf2342d9ce1f862a7239208c0c3d4d41aea05
robin* <robin_maki@planet.moe>
2018-05-15 20:51:59 +0900

    프로필 모델에 필요한 칼럼들 다 넣음

--- src/models/profile.js
+++ src/models/profile.js
@@ -5,7 +5,18 @@ const Account = require('./account');
 const model = db.define('profile', {
 	id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
 	name: {type: Sequelize.STRING(20), unique: true},
-	domain: {type: Sequelize.STRING}
+	domain: {type: Sequelize.STRING},
+	displayName: {type: Sequelize.STRING},
+	locked: {type: Sequelize.BOOLEAN, defaultValue: false},
+	note: {type: Sequelize.TEXT, defaultValue: ''},
+	url: {type: Sequelize.STRING(512)},
+	avatar: {type: Sequelize.UUID},
+	avatarStatic: {type: Sequelize.UUID},
+	header: {type: Sequelize.UUID},
+	headerStatic: {type: Sequelize.UUID},
+	followerCount: {type: Sequelize.INTEGER, defaultValue: 0},
+	followingCount: {type: Sequelize.INTEGER, defaultValue: 0},
+	statusesCount: {type: Sequelize.INTEGER, defaultValue: 0}
 }, {
 	indexes: [{fields: ['domain']}]
 });
